f4b50f4435426d7a2dd27563aa028ffbc0750615
jeevanthask <jeevanthanandika@gmail.com>
2018-10-07 08:19:35 +0530

    ArgumentPractise2

---
+++ ArgumentPractise2.py
@@ -0,0 +1,7 @@
+def printAction(name = "nandika",action = "eat",item = "apples"):
+    print(name,action,item)
+
+printAction()
+printAction("soyya","bath","coconut oil")
+printAction(item = "dhal")
+printAction(action = "ride",name = "jeevantha",item = "bicycle")
