90ce562f25e02f6d04fd594468262bb31f887a97
jeevanthask <jeevanthanandika@gmail.com>
2018-10-07 08:19:03 +0530

    ArgumentPractise1

---
+++ ArgumentPractise1.py
@@ -0,0 +1,13 @@
+
+def printGender(sex = "unknown"):
+    if sex is "male":
+        sex = "male"
+    elif sex is "female":
+        sex = "female"
+    print("your gender is",sex)
+
+
+
+printGender()
+printGender("male")
+printGender("female")
